24e9072f493f735ffe8e678d7af4c7d3c351b62f
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 21:38:11 +0530

    auto-update-main.js(21:38:07)

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ function matrix () {
 
   
   for(i=0;i<screen.width;i+=20){ 
-        ctx.fillText(String.fromCharCode(Math.random() * 122), i, 100);
+        ctx.fillText(String.fromCharCode(Math.random() * 122), i, i);
   }
   c=Math.random() * screen.height
 
