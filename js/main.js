b0f5f54aee4eebc449af2a2f469568bebfaa7c38
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 22:32:21 +0530

    auto-update-main.js(22:32:20)

--- js/main.js
+++ js/main.js
@@ -23,11 +23,12 @@ maximize(canvas)
 ctx.fillStyle = '#000';
 ctx.fillRect(0, 0, w, h);
 
-const cols = Math.floor(w / 20) + 1;
-const ypos = Array(cols).fill(0);
+
 
 function matrix () {
- 
+  
+
+    
   ctx.fillStyle = '#0001';
   ctx.fillRect(0, 0, w, h);
 
