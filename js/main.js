b1c78d159ddb86e0d2ef974399e025a3eeac4fe3
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 21:29:10 +0530

    auto-update-main.js(21:29:07)

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ function matrix () {
     const text = String.fromCharCode(Math.random() * 122);
     const x = ind * 20;
     ctx.fillText(text, x, y);
-    con
+    console.log(x,y)
     if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
     else ypos[ind] = y + 20;
   });
