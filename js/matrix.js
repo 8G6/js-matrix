efa0ccb6bd919108c56d9d6b672143585aa85a60
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 20:21:38 +0530

    auto-update-matrix.js(20:21:35)

--- js/matrix.js
+++ js/matrix.js
@@ -112,7 +112,7 @@ class Matrix{
 mat = new Matrix('canv')
 mat.fontSize    = '15pt'
 mat.maximize    = 1
-mat.rain        = 1
+mat.rain        = 0
 mat.fadeOut     = false
 mat.randomColor = false
 mat.itrate()
