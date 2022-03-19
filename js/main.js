2f0c20d1e60502a2c018019bd4910ff77b631a4a
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 21:33:21 +0530

    auto-update-main.js(21:33:20)

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ function matrix () {
   const text = String.fromCharCode(Math.random() * 122);
   for(i=0;i<screen.width;i+=20){
         for(j=0;j<c;j++){
-            ctx.fillText(text, j, i);
+            ctx.fillText(text, j*10, i*10);
         }
   }
   c++
