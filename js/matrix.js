dee03fb3175ef347aecf80b5efcea857eb391a3e
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 20:03:36 +0530

    auto-update-matrix.js(0)

--- js/matrix.js
+++ js/matrix.js
@@ -102,6 +102,20 @@ mat.fadeOut     = false
 mat.randomColor = false
 mat.itrate()
 
+mat = new Matrix('canv')
+mat.fontSize    = '15pt'
+mat.maximize    = 1
+mat.fadeOut     = false
+mat.randomColor = false
+mat.itrate()
+
+mat = new Matrix('canv')
+mat.fontSize    = '15pt'
+mat.maximize    = 1
+mat.fadeOut     = false
+mat.randomColor = false
+mat.itrate()
+
 
 
 
