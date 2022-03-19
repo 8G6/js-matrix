e87063c22cf74b6376604b513d22eb973d9c8dca
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 22:16:08 +0530

    auto-update-matrix.js(22:16:07)

--- js/matrix.js
+++ js/matrix.js
@@ -90,7 +90,7 @@ class Matrix{
     Rain=()=>{
         this.fadeEffect()
         let i,j;
-        for(i=0;i<screen.width;i++){
+        for(i=0;i<screen.height;i++){
             this.ctx.fillStyle     = setColor(this.fontColor,this.randomColor)
             this.ctx.fillText(rand(this.charArray),i,i)
         }
