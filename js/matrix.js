824d181fc7c2fd7a68f550b1b2347b95f7ac17f2
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 21:20:48 +0530

    auto-update-matrix.js(21:20:48)

--- js/matrix.js
+++ js/matrix.js
@@ -79,7 +79,7 @@ class Matrix{
             this.ctx.fillText(text, x, y);
             this.ctx.fillStyle     = setColor(this.fontColor,this.randomColor)
             if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
-            else y+= y + 20;
+            else yy + 20;
         });
     }
     
