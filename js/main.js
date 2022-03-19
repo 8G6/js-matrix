8703a04112505c2086543eef6169904c7b8aaa9e
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 20:29:15 +0530

    auto-update-main.js(20:29:11)

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ function maximize(canvas){
     canvas.height = document.body.offsetHeight;
     
 }
-ma
+maximize(canvas)
 function matrix () {
     ctx.fillStyle = '#0001';
     ctx.fillRect(0, 0, w, h);
