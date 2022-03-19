b8b146b5cf6c0571afbcf935952d75288aa59734
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 21:28:25 +0530

    auto-update-main.js(21:28:19)

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ const canvas = document.getElementById('canv');
 const ctx = canvas.getContext('2d');
 
 
-const w = canvas.width = screen.;
+const w = canvas.width = screen.width;
 const h = canvas.height = document.body.offsetHeight;
 
 maximize(canvas)
